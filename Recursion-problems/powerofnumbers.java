9d17c41001eee0878c9bffd01f5b4eab961d596b
RobbinThakur <101439452+RobbinThakur@users.noreply.github.com>
2023-07-26 21:37:37 +0530

    <>

--- Recursion-problems/powerofnumbers.java
+++ Recursion-problems/powerofnumbers.java
@@ -1,19 +1,22 @@
 package recursion;
 
 public class powerofnumbers {
+	public static int power(int a,int b) {
+		if(b == 0) {
+			return 1;
+		}
+		else {
+			return a*power(a,b-1);
+		}
+
 	
-	static int power(int a,int b) {
-		if(b == 0) return 1;
-		 
-	
-		int subproblem = (a^b-1);
 		
-		return subproblem*a;
 	}
 	
 	public static void main(String[] args) {
 		
-		power(2,3);
+		int ans = power(2,3);
+		System.out.println(ans);
 	}
 
 }
