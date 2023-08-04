4971e8bd6ceb8152d901c662172c6de9d917b650
RobbinThakur <101439452+RobbinThakur@users.noreply.github.com>
2023-08-04 21:13:59 +0530

    <>

---
+++ Strings/Stringsbasics.java
@@ -0,0 +1,18 @@
+package strings;
+import java.util.*;
+
+public class Stringsbasics {
+	public static void main(String[] args) {
+		
+	
+	String str ="abcd";
+	String str1 = new String("qwerty");
+	
+	Scanner sc = new Scanner(System.in);
+	String name;
+	name = sc.nextLine();
+	
+	System.out.println(name);
+
+}
+}
