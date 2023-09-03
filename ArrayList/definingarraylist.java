8fa4d1a17426acbb84d9c63522f5abfbdcd52399
RobbinThakur <101439452+RobbinThakur@users.noreply.github.com>
2023-09-03 20:42:14 +0530

    <>

---
+++ ArrayList/definingarraylist.java
@@ -0,0 +1,15 @@
+
+
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class definingarraylist {
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		ArrayList<String> list2 = new ArrayList<>();
+		ArrayList<Boolean> list3 = new ArrayList<>();
+		}
+
+}
