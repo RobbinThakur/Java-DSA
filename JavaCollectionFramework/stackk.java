64413d03da6cbbf6a5e2fc56c4b1c0a43dc41ddb
RobbinThakur <101439452+RobbinThakur@users.noreply.github.com>
2023-10-03 22:39:17 +0530

    <>

---
+++ JavaCollectionFramework/stackk.java
@@ -0,0 +1,23 @@
+package javacollectionframework;
+
+import java.util.*;
+
+public class stackk {
+	
+	public static void main(String[] args) {
+		Stack<Integer> s = new Stack<>();
+		
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		
+		while(!s.isEmpty()) {
+			System.out.println(s.peek());
+			s.pop();
+		}
+		
+	}
+	
+	
+
+}
